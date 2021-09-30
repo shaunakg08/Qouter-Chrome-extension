2cfd162c7836c1b3d297a1ecbd4c3da1ebc143f5
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-09-30 11:06:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const btn = document.querySelector("#myBtn");
+
+fetch("https://quotable.io/random")
+    .then(res => res.json())
+    .then(data => {
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author
+    })
