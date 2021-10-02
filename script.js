3303c8132642c602dd38d33c2bf62b25b407b5ef
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-10-02 12:05:58 +0530

    updated author name

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ fetch("https://quotable.io/random")
     .then(res => res.json())
     .then(data => {
         quote.innerHTML = data.content;
-        author.innerHTML = data.author
+        author.innerHTML = "~ " + data.author
     })
